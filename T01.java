6da91bfb551b97c10a5af558e5a5bec53d5473d5
keziapasaribu <kanayamijelia@gmail.com>
2024-09-04 21:27:25 +0700

    Sudah selesai

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
+// 12S24009 - Kezia Vania Pasaribu
 // NIM - NAMA
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
